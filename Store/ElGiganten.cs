bb392ec2c0c40325717546fbbc3f355532575513
Fakhara <fakhara.imran82@gmail.com>
2024-10-02 12:20:38 +0200

    Add project files.

---
+++ Store/ElGiganten.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    //subclass ElGiganten
+    public class ElGiganten : Store
+    {
+        public ElGiganten() : base("ElGiganten" , new TimeOnly(9,0), new TimeOnly(22,0) {  }
+
+        public override List<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product("Laptop", 15000m),
+                new Product("Smartphone", 8000m),
+                new Product("Television", 12000m)
+             
+            };
+        }
+
+    }
+}
---
+++ Store/Store.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public TimeOnly OpeningTime { get; set; }
+        public TimeOnly ClosingTime { get; set; }
+
+        public Store(string name, TimeOnly openingTime, TimeOnly closingTime)
+        {
+            Name = name;
+            OpeningTime = openingTime;
+            ClosingTime = closingTime;
+        }
+        public virtual List<Product> GetProducts()
+        {
+            // Either return an empty list or throw an exception to indicate it must be overridden
+            throw new NotImplementedException("GetProducts method should be overridden in derived classes.");
+        }
+
+    }
+}
